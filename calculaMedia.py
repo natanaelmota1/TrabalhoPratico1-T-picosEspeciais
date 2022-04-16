59b42c157517e57d0ff27f71d51dfc4e2c3fdbee
Natanael da Mota Figueira <54650675+natanaelmota1@users.noreply.github.com>
2022-04-15 22:58:58 -0400

    Add files via upload

---
+++ calculaMedia.py
@@ -0,0 +1,32 @@
+def calculaMedia(ap1, ap2):
+    #Tratando casos de entradas inválidas
+    try:
+        ap1 = float(ap1)
+        ap2 = float(ap2)
+    except ValueError:
+        return("Entrada inválida, a entrada não é um número.")
+
+    if(ap1 > 10):
+        return("Entrada inválida, a AP1 tem valor maior que 10.")
+    elif(ap1 < 0):
+        return("Entrada inválida, a AP1 tem valor negativo.")
+    elif(ap2 > 10):
+        return("Entrada inválida, a AP2 tem valor maior que 10.")
+    elif(ap2 < 0):
+        return("Entrada inválida, a AP2 tem valor negativo.")
+    
+    #fazendo cálculo da média
+    media = (ap1 + ap2) / 2
+    if (media >= 8):
+        return("Aluno aprovado.")
+    elif (media < 4):
+        return("Aluno reprovado.")
+    else:
+        return("Aluno precisa fazer PF.")
+    
+ap1, ap2 = input().split(" ")
+ap1 = float(ap1)
+ap2 = float(ap2)
+
+    
+    
